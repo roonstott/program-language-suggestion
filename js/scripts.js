5ce5987ec01f072c097a50d3db3e3d5a770da5a9
Robert Onstott <roonstott@gmail.com>
2022-08-27 14:25:52 -0700

    Complete 1 to 7 survery with all question slots. Remove stand in vegetable categories.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,68 @@
+
+//function called on "load"
+
+window.addEventListener("load", function() {
+  const jsForm = document.getElementById("js-select-form");
+  jsForm.addEventListener("submit", displayJsSuggestion);
+   
+  document.getElementById("show-me").addEventListener("click", function() {
+    submitForms();
+    displayBackEndSuggestion();
+  });
+
+});
+
+//function to submit all 7 radio-button forms, called by eventListener on bottom section "click" button
+
+function submitForms() {
+  const q1Form = document.getElementById("q1");
+  const q2Form = document.getElementById("q2");
+  const q3Form = document.getElementById("q3");
+  const q4Form = document.getElementById("q4");
+  const q5Form = document.getElementById("q5");
+  const q6Form = document.getElementById("q6");
+  const q7Form = document.getElementById("q7");
+
+  q1Form.requestSubmit();
+  q1Form.addEventListener("submit" , function(event) {
+    event.preventDefault();
+  });
+
+  q2Form.requestSubmit();
+  q2Form.addEventListener("submit" , function(event) {
+    event.preventDefault();
+  });
+
+  q3Form.requestSubmit();
+  q3Form.addEventListener("submit" , function(event) {
+    event.preventDefault();
+  });
+
+  q4Form.requestSubmit();
+  q4Form.addEventListener("submit" , function(event) {
+    event.preventDefault();
+  });
+
+  q5Form.requestSubmit();
+  q5Form.addEventListener("submit" , function(event) {
+    event.preventDefault();
+  });
+
+  q6Form.requestSubmit();
+  q6Form.addEventListener("submit" , function(event) {
+    event.preventDefault();
+  });
+
+  q7Form.requestSubmit();
+  q7Form.addEventListener("submit" , function(event) {
+    event.preventDefault();
+  });
+}
+
+//display functions
+
+  //top section: JS
+
 function displayJsSuggestion(event) {
   event.preventDefault();
   const jsResponse = document.getElementById("js-yes/no").value;
@@ -13,27 +78,22 @@ function displayJsSuggestion(event) {
   document.getElementById("js-response").innerText= jsMessage
 }
 
+  //bottom section: back-end
+
 function displayBackEndSuggestion() {
-  const asparagusAnswer = document.querySelector("input[name='asparagus']:checked").value;
-  document.getElementById("back-end-response").innerText = asparagusAnswer;
+  const q1Answer = parseInt(document.querySelector("input[name='q1']:checked").value);
+  const q2Answer = parseInt(document.querySelector("input[name='q2']:checked").value);
+  const q3Answer = parseInt(document.querySelector("input[name='q3']:checked").value);
+  const q4Answer = parseInt(document.querySelector("input[name='q4']:checked").value);
+  const q5Answer = parseInt(document.querySelector("input[name='q5']:checked").value);
+  const q6Answer = parseInt(document.querySelector("input[name='q6']:checked").value);
+  const q7Answer = parseInt(document.querySelector("input[name='q7']:checked").value);
+  
+  let tally = (q1Answer + q2Answer + q3Answer + q4Answer + q5Answer + q6Answer + q7Answer);
+  
+  document.getElementById("back-end-response").innerText = (tally.toString());
 }
 
 
-function submitForms() {
-  const asparagusForm = document.getElementById("asparagus");
-  asparagusForm.addEventListener("submit" , function(event) {
-    event.preventDefault();
-  });
-  asparagusForm.requestSubmit();
-  }
 
-window.addEventListener("load", function() {
-  const jsForm = document.getElementById("js-select-form");
-  jsForm.addEventListener("submit", displayJsSuggestion);
-   
-  document.getElementById("show-me").addEventListener("click", function() {
-    submitForms();
-    displayBackEndSuggestion();
-  });
 
-});
