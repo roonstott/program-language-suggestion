b6d479fcef176bd52dd3c87340106b3717f4e26d
Robert Onstott <roonstott@gmail.com>
2022-08-27 12:24:28 -0700

    Work on create single button to submit multiple forms. Work in progress.

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,31 @@ function displayJsSuggestion(event) {
   else {
     jsMessage = "You should definitely learn Javascript. It is essential to all web development, whether it's front end or back end. Plus... it's a great entrypoint for encountering the underlying principles that span across all coding languages." 
   }
-  
+
   document.getElementById("js-response").innerText= jsMessage
 }
 
+function displayBackEndSuggestion() {
+  const asparagusAnswer = document.querySelector("asparagus#input[name='asparagus']:checked").value;
+  document.getElementById("back-end-response").innerText = asparagusAnswer;
+}
 
 
+function submitForms() {
+  const asparagusForm = document.querySelector("form#asparagus");
+  asparagusForm.submit();
+  asparagusForm.addEventListener("submit" , function(event) {
+    event.preventDefault();
+  });
+  }
 
 window.addEventListener("load", function() {
   const jsForm = document.getElementById("js-select-form");
   jsForm.addEventListener("submit", displayJsSuggestion);
+   
+  document.getElementById("show-me").addEventListener("click", function() {
+    submitForms();
+    displayBackEndSuggestion();
+  });
+
 });
